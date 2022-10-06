433134e46df41cafcad6cd90a9cc1e779663fa87
Kézia Ketillen <k.kezia@academico.ifrn.edu.br>
2022-10-06 20:56:07 -0300

    add solução/comentarios/descrição de Idade de Camila

--- Junior/IdadeDeCamila.java
+++ Junior/IdadeDeCamila.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class IdadeDeCamila
 {
-	public static void main(String[] args) {
-	    
-		Scanner leia = new Scanner(System.in);
-		
-		int idade1 = leia.nextInt();
-		int idade2 = leia.nextInt();
-		int idade3 = leia.nextInt();
-		
-		int somaIdades = idade1 + idade2 + idade3;
-		
-		int maiorIdade = Math.max(idade1, Math.max(idade2, idade3)); //encontrar a maior idade entre as 3 idades
+    public static void main(String[] args) {
+        
+        Scanner leia = new Scanner(System.in);
+        
+        int idade1 = leia.nextInt();
+        int idade2 = leia.nextInt();
+        int idade3 = leia.nextInt();
+        
+        int somaIdades = idade1 + idade2 + idade3;
+        
+        int maiorIdade = Math.max(idade1, Math.max(idade2, idade3)); //encontrar a maior idade entre as 3 idades
         int menorIdade = Math.min(idade1, Math.min(idade2, idade3)); //encontrar a menor idade entre as 3 idades
         
         int meio = somaIdades - (maiorIdade + menorIdade); //Idade de Camila
         
         System.out.println(meio);
         
-	}
+    }
 }
