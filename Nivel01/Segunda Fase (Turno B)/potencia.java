03576a3cef9a62859bb096d464c4d74b6c0ebd53
Tiago Rodrigues <tiagords127@gmail.com>
2022-10-15 17:09:22 -0300

    Add comentários

--- Nivel01/Segunda Fase (Turno B)/potencia.java
+++ Nivel01/Segunda Fase (Turno B)/potencia.java
@@ -12,18 +12,23 @@ public class potencia {
 			String t = scan.next();
 
 			String[] expressao = new String[2];
-			
-			int ultimoCharIndex = t.length() - 1;
-			
-			expressao[0] = String.valueOf(t.charAt(ultimoCharIndex));
-			expressao[1] = t.substring(0, ultimoCharIndex);
-			
 
+			int ultimoCharIndex = t.length() - 1;
+
+			// Pega o último char de t, convertido em String
+			expressao[0] = String.valueOf(t.charAt(ultimoCharIndex));
+
+			// Copia toda a expressão (t), com exceção do último char 
+			expressao[1] = t.substring(0, ultimoCharIndex);
+
+			// Converte os valor em int
 			int potencia = Integer.parseInt(expressao[0]);
 			int num = Integer.parseInt(expressao[1]);
-			
+
+			// Realiza a operação de potência
 			double valor = Math.pow(num, potencia);
-			
+
+			// Soma as potências
 			soma += valor;
 		}
 		
